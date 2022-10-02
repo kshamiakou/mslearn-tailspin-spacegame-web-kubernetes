8093fdaebdac16c391961569d17b120c61604735
kshamiakou <110040747+kshamiakou@users.noreply.github.com>
2022-10-02 21:16:47 +0200

    upd

--- Tailspin.SpaceGame.LeaderboardContainer/Startup.cs
+++ Tailspin.SpaceGame.LeaderboardContainer/Startup.cs
@@ -34,10 +34,8 @@ namespace Tailspin.SpaceGame.LeaderboardContainer
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
+            app.UseDeveloperExceptionPage();
+
 
             app.UseRouting();
 
