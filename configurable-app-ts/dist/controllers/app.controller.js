7eec46f9ba18e8207324364427fbfd9a4d9325ec
kshamiakou <110040747+kshamiakou@users.noreply.github.com>
2022-10-05 14:50:23 +0200

    upd

--- configurable-app-ts/dist/controllers/app.controller.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.handleRootRequest = void 0;
-const handleRootRequest = (request, response) => {
-    response.send({ gagaga: 1 });
-};
-exports.handleRootRequest = handleRootRequest;
--- configurable-app-ts/dist/routes.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const app_controller_1 = require("./controllers/app.controller");
-exports.default = (app) => {
-    app.get("/", app_controller_1.handleRootRequest);
-};
